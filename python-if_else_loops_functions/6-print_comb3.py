4839f53a271302da070ed0e1a515d58373ebc02b
Gabriel Rossy <grossy.sjb@gmail.com>
2023-09-19 16:29:19 +0000

    task6 fixed

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for x in range(0,8):
-    for y in range(1,10):
+for x in range(0, 8):
+    for y in range(1, 10):
         if x != y:
             if y > x:
                 print("{}".format(str(x) + str(y)), end=', ')
