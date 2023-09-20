b795b4366e557932d3faf6e1682661411604ae49
Gabriel Rossy <grossy.sjb@gmail.com>
2023-09-20 15:31:57 +0000

    task8 changing method

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    result = []
+    result = ""
     for char in str:
         upper = ord(char)
         if (96 < upper < 123):
             upper -= 32
-        result.append(chr(upper))
-    print("".join(result))
+        result += chr(upper)
+    print("{}".format(result))
