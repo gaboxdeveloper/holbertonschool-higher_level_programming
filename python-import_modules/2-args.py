77cac11d424ced94fb2532c301b93196c5755513
Gabriel Rossy <grossy.sjb@gmail.com>
2023-09-21 14:23:20 +0000

    task2 fixed

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,12 +4,12 @@ if __name__ == "__main__":
     lenarg = len(argv)
     i = 1
     if lenarg > 2:
-        print(f"{lenarg - 1}: arguments")
+        print(f"{lenarg - 1} arguments:")
         while i < lenarg:
             print(f"{i}: {argv[i]}")
             i += 1
     elif lenarg == 1:
-        print(f"0: arguments")
+        print(f"0 arguments.")
     elif lenarg == 2:
-        print(f"1: argument")
+        print(f"1 argument:")
         print(f"1: {argv[1]}")
