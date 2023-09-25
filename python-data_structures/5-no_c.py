b1b2c3c12ea91d47ab447fd50eaacf27370852b9
Gabriel <grossy.sjb@gmail.com>
2023-09-24 17:00:42 -0700

    task5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     new_string = ""
     for let in my_string:
-        if let != 'c' and != 'C':
+        if let != 'c' or != 'C':
             new_string += let
     return new_string
