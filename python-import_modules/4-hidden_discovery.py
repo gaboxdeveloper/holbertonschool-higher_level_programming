e510b0ac13303f48b48176a31ebdedccd4cbb6c8
Gabriel <grossy.sjb@gmail.com>
2023-09-24 18:58:03 -0700

    task4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    def print_module_names(hidden_4):
-        module_names = dir(hidden_4)
-        for name in sorted(module_names):
-            if not name.startswith('__'):
-                print(name)
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
