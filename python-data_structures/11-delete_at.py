c970d23ec6c93d0fb5400c6efe6794baf2e724b7
Gabriel <grossy.sjb@gmail.com>
2023-09-24 18:43:45 -0700

    task11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -6,5 +6,5 @@ def delete_at(my_list=[], idx=0):
     new_list = []
     for i in my_list:
         if i != idx:
-            new_list += idx
+            new_list.append(my_list[i])
     return new_list
