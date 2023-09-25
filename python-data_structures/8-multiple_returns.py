97c9e126a763035041df1ce8bdbbec708a333aac
Gabriel <grossy.sjb@gmail.com>
2023-09-24 18:28:26 -0700

    task8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     largo = len(sentence)
-    if sentence == None:
+    if not sentence:
         pchar = None
     else:
         for i in sentence:
             pchar = sentence[0]
-    return largo pchar
+    return largo, pchar
