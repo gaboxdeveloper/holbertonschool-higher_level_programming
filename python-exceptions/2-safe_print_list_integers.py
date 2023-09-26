7e108ebe6db60ed5be255c2e268b296ad125f899
Gabriel <grossy.sjb@gmail.com>
2023-09-26 15:28:07 -0700

    task2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    printed = 0
+    try:
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end='')
+                printed += 1
+    except Exception:
+        pass
+    print()
+    return printed
