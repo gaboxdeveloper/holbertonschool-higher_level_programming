445568600f1e13f94ff8477eea79d38f63896142
Gabriel <grossy.sjb@gmail.com>
2023-09-26 15:56:31 -0700

    task0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     printed = 0
     try:
         for i in range(x):
-            print(my_list[i], end='')
+            print("{}".format(my_list[i]), end='')
             printed += 1
         print("")
     except Exception:
