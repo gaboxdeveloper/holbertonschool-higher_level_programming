c2befea2cc63d98e18e11152a163318e1471e375
Gabriel <grossy.sjb@gmail.com>
2023-09-26 16:06:42 -0700

    task3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    r = None
     try:
         r = a / b
         return r
     except ZeroDivisionError:
         return None
     finally:
-        print(r)
+        print("Inside result: {}".format(r))
