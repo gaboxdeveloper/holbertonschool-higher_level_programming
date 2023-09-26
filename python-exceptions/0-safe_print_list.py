5b26f51b93b60c94e50c4488c50b976134a4c94b
Gabriel <grossy.sjb@gmail.com>
2023-09-26 11:08:09 -0700

    first try

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(x):
+            if i == x - 1:
+                print(my_list[i])
+            else:
+                print(my_list[i], end='')
+        return x
+    except:
+        print("")
+        return i
