6810fd3226c6fc4cc159c4717aa53fff00645d0e
Gabriel <grossy.sjb@gmail.com>
2023-09-26 08:43:30 -0700

    task12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,19 +3,24 @@ def roman_to_int(roman_string):
     if roman_string is None:
         return 0
     retorno = 0
-    for num in roman_string:
-        if num == "I":
-            retorno += 1
-        if num == "V":
+    for num in range(len(roman_string)):
+        if roman_string[num] == "I":
+            if num == (len(roman_string) - 1):
+                retorno += 1
+            elif roman_string[num + 1] == "V" or roman_string[num + 1] == "X":
+                retorno -= 1
+            else:
+                retorno += 1
+        if roman_string[num] == "V":
             retorno += 5
-        if num == "X":
+        if roman_string[num] == "X":
             retorno += 10
-        if num == "L":
+        if roman_string[num] == "L":
             retorno += 50
-        if num == "C":
+        if roman_string[num] == "C":
             retorno += 100
-        if num == "D":
+        if roman_string[num] == "D":
             retorno += 500
-        if num == "M":
+        if roman_string[num] == "M":
             retorno += 1000
     return retorno
