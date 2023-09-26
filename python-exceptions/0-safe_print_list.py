3e86fac714b041b540ede70d094dc8789e666fdf
Gabriel <grossy.sjb@gmail.com>
2023-09-26 15:44:50 -0700

    task0 y 2

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end='')
             printed += 1
+        print("")
     except Exception:
-        pass
-    print()
+        print("")
     return printed
