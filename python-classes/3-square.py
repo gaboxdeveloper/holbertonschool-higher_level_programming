86646e206b7834db8a27b356cc8f7d9a4828817c
Gabriel <grossy.sjb@gmail.com>
2023-09-30 19:12:09 +0000

    task3

---
+++ python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Class called square"""
+
+
+class Square:
+    """This class is made
+    to create a square"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+            self._area = size ** 2
+    def area(self):
+        return self._area
