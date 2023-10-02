3c8a79ff55d72611dc7faaa14d26ba2169de74a5
Gabriel <grossy.sjb@gmail.com>
2023-10-02 23:52:39 +0000

    task6 fixed pycode0

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle:
     Those are private attributes, but we can modify them
     with the setter"""
     number_of_instances = 0
-    
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
