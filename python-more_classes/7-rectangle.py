77707d10cfeeb26734b49652b77efc99f2bb1ff9
Gabriel <grossy.sjb@gmail.com>
2023-10-02 22:55:08 +0000

    task7

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -10,7 +10,7 @@ class Rectangle:
     with the setter"""
     number_of_instances = 0
     print_symbol = "#"
-    
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
