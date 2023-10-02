01fd9ad8d658da0779a37b74aa170b766f1f766e
Gabriel <grossy.sjb@gmail.com>
2023-10-02 18:50:28 +0000

    task1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Module where we will
+make a class to create a rectangle"""
+
+
+class Rectangle:
+    """An empty reclangle"""
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.width
+
+    @width.setter
+    def width(self, value):
+        if value is not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+    
+    @property
+    def height(self):
+        return self.width
+
+    @width.setter
+    def height(self, value):
+        if value is not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__height = value
