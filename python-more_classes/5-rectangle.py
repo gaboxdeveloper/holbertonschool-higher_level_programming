9bd15b6254bb3624c2738842840cfc3209363910
Gabriel <grossy.sjb@gmail.com>
2023-10-02 22:25:32 +0000

    task5

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -56,5 +56,5 @@ class Rectangle:
     def __repr__(self):
         return f"Rectangle({self.__width}, {self.__height})"
 
-    def __del__():
+    def __del__(self):
         print("Bye rectangle...")
