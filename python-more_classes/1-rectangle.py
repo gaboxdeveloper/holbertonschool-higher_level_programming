5d99f03f495513d83705116e1c044cb9e52b8276
Gabriel <grossy.sjb@gmail.com>
2023-10-02 18:58:43 +0000

    task1 fixed

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
     Those are private attributes, but we can modify them
     with the setter"""
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
