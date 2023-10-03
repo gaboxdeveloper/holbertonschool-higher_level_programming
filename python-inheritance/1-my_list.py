af6de57f372a236787e555800e0d30313d2388f0
Gabriel <grossy.sjb@gmail.com>
2023-10-03 21:53:08 +0000

    task7 adding a new test

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,7 +3,8 @@
 
 
 class MyList(list):
-    """class from list"""
+    """class inherited
+    from the list class"""
     def print_sorted(self):
         """Print the list elements in ascending sorted order."""
         sorted_list = sorted(self)
