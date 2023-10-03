f978c317c748b3f25e8f6b00044c9ddf2b84b8e9
Gabriel <grossy.sjb@gmail.com>
2023-10-03 22:20:13 +0000

    task9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""module of a rectangle!"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """class Rectangle that inherits
+    from BaseGeometry"""
+    def __init__(self, width, height):
+        """constructor of rectangle"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """return the area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
