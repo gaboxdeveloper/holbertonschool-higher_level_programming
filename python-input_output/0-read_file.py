e2682980eb5d2d0343534803ad3625f2d4ecac42
Gabriel <grossy.sjb@gmail.com>
2023-10-04 23:42:08 +0000

    task0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Write a function that reads a text file (UTF8)
+and prints it to stdout:
+Prototype: def read_file(filename=""):
+You must use the with statement
+You don't need to manage file permission or file doesn't exist exceptions."""
+
+
+def read_file(filename=""):
+    """function that reads a text file (UTF8)
+    and prints it to stdout"""
+    with open(filename, encoding="utf-8") as openfile:
+        read_data = openfile.read()
