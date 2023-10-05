b69507dfaabe7240db676cadd9591810c6cd33ba
Gabriel <grossy.sjb@gmail.com>
2023-10-05 13:12:00 +0000

    task0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,5 +9,9 @@ You don't need to manage file permission or file doesn't exist exceptions."""
 def read_file(filename=""):
     """function that reads a text file (UTF8)
     and prints it to stdout"""
-    with open(filename, encoding="utf-8") as openfile:
-        read_data = openfile.read()
+    try:
+        with open(filename, encoding="utf-8") as openfile:
+            for line in openfile:
+                print(line, end='')
+    except FileNotFoundError:
+        pass
