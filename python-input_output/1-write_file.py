154382afff17841428b7bc637878d0d06a756c5f
Gabriel <grossy.sjb@gmail.com>
2023-10-06 14:52:09 +0000

    task1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,6 +5,6 @@
 
 def write_file(filename="", text=""):
     """function that writes a string to a text file"""
-    with open(filename, encoding="utf-8") as f:
-        f.write(f)
-        f.close(f)
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(text)
+    f.close()
