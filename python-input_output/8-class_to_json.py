f90b0e5a1a64c45660073708eb5a29342f540b3c
Gabriel <grossy.sjb@gmail.com>
2023-10-07 22:18:01 +0000

    task8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -8,6 +8,10 @@ integer and boolean) for JSON serialization of an object:
 
 def class_to_json(obj):
     """function that returns the dictionary description"""
+    if isinstance(obj, (dict, str, int, bool)):
+        return obj
+    if isinstance(obj, (list, tuple)):
+        return [class_to_json(item) for item in obj]
     if hasattr(obj, '__dict__'):
         serializable_dict = {}
         for key, value in obj.__dict__.items():
