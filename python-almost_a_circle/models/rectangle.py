80fc89b48d6bb4620b96ed5f4a48b65d44726c87
Gabriel <grossy.sjb@gmail.com>
2023-10-10 14:46:11 +0000

    task6

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -85,4 +85,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """return rectangle representation"""
-        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y}\
+            - {self.width}/{self.height}"
