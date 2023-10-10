3f980681c9b7646099b30a745b01a8799c6abd3d
Gabriel <grossy.sjb@gmail.com>
2023-10-10 13:14:23 +0000

    task 1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+lsadgfshgjhdfhdgseasrdtfjyfjthrgaestr\
+    dgfesasrgdhftrgserawestrdytfrte
+"""
+
+
+class Base:
+    """clase de base"""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        self.__nb_objects += 1
