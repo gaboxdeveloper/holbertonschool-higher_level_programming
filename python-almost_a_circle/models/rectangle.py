a76056f5ea6f459845297044e2a1867865005470
Gabriel <grossy.sjb@gmail.com>
2023-10-10 13:52:40 +0000

    task 2

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+lsadgfshgjhdfhdgseasrdtfjyfjthrgaestr\
+    dgfesasrgdhftrgserawestrdytfrte
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """xdfgchvjklljhgfhjkl"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        """getter"""
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        """setter"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """x getter"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """x setter"""
+        self.x = value
+
+    @property
+    def y(self):
+        """x getter"""
+        return self.__y
+    
+    @y.setter
+    def y(self, value):
+        """y setter"""
+        self.__y = value
