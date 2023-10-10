456fc06cb0e1151e72fd98f2fe471ef6f3656b79
Gabriel <grossy.sjb@gmail.com>
2023-10-10 13:59:38 +0000

    task2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -10,8 +10,8 @@ class Rectangle(Base):
     """xdfgchvjklljhgfhjkl"""
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         self.__x = x
         self.__y = y
 
