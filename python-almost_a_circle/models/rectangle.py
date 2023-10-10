579b9dde8064b737ede472289ac053c07cd108e8
Gabriel <grossy.sjb@gmail.com>
2023-10-10 14:00:42 +0000

    task2 v3

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,6 +9,7 @@ from models.base import Base
 class Rectangle(Base):
     """xdfgchvjklljhgfhjkl"""
     def __init__(self, width, height, x=0, y=0, id=None):
+        """yuitueryetawtrtyueyryet"""
         super().__init__(id)
         self.width = width
         self.height = height
