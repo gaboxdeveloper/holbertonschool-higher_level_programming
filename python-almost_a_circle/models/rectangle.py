b39febfb668c5ffcb1982a0fefd0a6910f09f8cb
Gabriel <grossy.sjb@gmail.com>
2023-10-11 13:57:17 +0000

    task7

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -78,10 +78,10 @@ class Rectangle(Base):
 
     def display(self):
         """display the rectangle"""
-        rect_str = ""
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            rect_str += str("#") * self.__width + "\n"
-        print(rect_str.rstrip("\n"))
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """return rectangle representation"""
