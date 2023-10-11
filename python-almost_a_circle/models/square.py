10cad1547b8a12498e71302da99a0625e1446ce4
Gabriel <grossy.sjb@gmail.com>
2023-10-11 15:38:39 +0000

    task10

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+lsadgfshgjhdfhdgseasrdtfjyfjthrgaestr\
+    dgfesasrgdhftrgserawestrdytfrte
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+        self.width = size
+        self.height = size
+
+    def __str__(self):
+        """return square representation"""
+        return f"[Square] ({self.id}) {self.x}/{self.y}"\
+            f" - {self.width}"
