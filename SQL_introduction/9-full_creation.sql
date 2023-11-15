f63209d3c2f3ff526c834f51a104bcc65af9212b
Gabriel <grossy.sjb@gmail.com>
2023-11-15 23:15:48 +0000

    task9 v2

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,11 +4,7 @@ CREATE TABLE IF NOT EXISTS second_table (
 	name VARCHAR(256),
 	score INT
 );
-INSERT INTO second_table (name, score)
-VALUES ('John',10)
-INSERT INTO second_table (name, score)
-VALUES ('Alex',3)
-INSERT INTO second_table (name, score)
-VALUES ('Bob',14)
-INSERT INTO second_table (name, score)
-VALUES ('George',8)
+INSERT INTO second_table (name, score) VALUES ('John',10);
+INSERT INTO second_table (name, score) VALUES ('Alex',3);
+INSERT INTO second_table (name, score) VALUES ('Bob',14);
+INSERT INTO second_table (name, score) VALUES ('George',8);
