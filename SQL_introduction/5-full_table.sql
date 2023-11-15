c8942b85015849fa24f82d59cd68cc80b54f99d1
Gabriel <grossy.sjb@gmail.com>
2023-11-15 22:26:25 +0000

    task5 v2

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
--- prints a description of the table first_table
-USE hbtn_0c_0;
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE
-FROM information_schema.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table'
+-- prints a description of the table
+SHOW CREATE TABLE 'first_table'
