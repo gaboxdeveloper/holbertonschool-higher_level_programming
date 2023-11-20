2aee6961cb073f4e788474c3eb26ae08c1a131d5
Gabriel <grossy.sjb@gmail.com>
2023-11-20 02:00:04 +0000

    task7

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+--creates the database hbtn_0d_usa and the table cities in that database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+    id INT UNIQUE PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    state_id INT FOREIGN KEY NOT NULL,
+    name VARCHAR(256) NOT NULL
+)
