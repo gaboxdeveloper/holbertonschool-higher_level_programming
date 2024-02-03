94e46b0c1d5e57d59ac2a175c25066772b5b5159
Gabriel <grossy.sjb@gmail.com>
2024-02-03 14:48:14 -0300

    task1

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,5 @@
+const header = document.querySelector('header');
+const redheader = document.getElementById('red_header');
+redheader.addEventListener('click', function() {
+  header.style.color = '#FF0000';
+});
